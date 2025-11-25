ad60cf466bbd3cd568d526247a0cd65446c9b90c
Isaac Hoyos <gamemasterx40@gmail.com>
2025-11-25 15:31:38 -0500

    Add main function for triangle area comparison

---
+++ Homework 4/Triangle area comparison (classes)/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Triangle.h"
+using namespace std;
+
+int main() {
+    Triangle triangle1;
+    Triangle triangle2;
+
+    double b, h;
+
+    // Read and set base and height for triangle1.
+    cin >> b >> h;
+    triangle1.SetBase(b);
+    triangle1.SetHeight(h);
+
+    // Read and set base and height for triangle2.
+    cin >> b >> h;
+    triangle2.SetBase(b);
+    triangle2.SetHeight(h);
+
+    cout << "Triangle with smaller area:" << endl;
+
+    // Compare the areas and output the triangle with the smaller area.
+    if (triangle1.GetArea() < triangle2.GetArea()) {
+        triangle1.PrintInfo();
+    }
+    else {
+        triangle2.PrintInfo();
+    }
+
+    return 0;
+}
